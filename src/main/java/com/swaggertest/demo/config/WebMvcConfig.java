e81573739b0f1342ee45e519c47aab0a0950f15e
nieminle <nieminle@tanzhou.cn>
2020-06-09 14:24:50 +0800

    新增log4j

--- src/main/java/com/swaggertest/demo/config/WebMvcConfig.java
+++ src/main/java/com/swaggertest/demo/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.swaggertest.demo.config;
 import com.swaggertest.demo.servlet.LoginHandlerInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
   /**
