1f8952727c3fa84e05052b0a0f9b38dc3e25b6d7
nieminle <nieminle@tanzhou.cn>
2020-12-23 17:50:20 +0800

    提交单例模式

---
+++ src/main/java/com/swaggertest/demo/design/Singleton.java
@@ -0,0 +1,19 @@
+package com.swaggertest.demo.design;
+
+/**
+ * 单例模式
+ */
+public class Singleton {
+
+    private static Singleton singleton;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+}
